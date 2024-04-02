4c178e9deb779d6f1c678ddfaf6822c87bdd7c48
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-04-02 19:14:41 +1000

    initial script

---
+++ app.py
@@ -0,0 +1,10 @@
+#!venv/bin/python
+
+if __name__ == "__main__":
+    print("test")
+    # argparser w help
+    #
+    # FUNCTIONS:
+    #   - GENERATE (env_var, key_unhashed)
+    #   - LIST (env_var)
+    #   - REMOVE (env_var, key_unhashed)
