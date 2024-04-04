c9fdd3928d1924b1001729b51cff226af981bcf7
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-04-05 09:12:06 +1000

    code cleanup

--- sync.sh
+++ sync.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Assign API KEY CSV values to API_KEY env variable (comma-separated)
+# Assign API KEY CSV values to the API_KEY env variable (comma-separated)
 
 csvfile="out/api-keys-db.csv"
 keys=""
 
 while IFS= read -r line; do
-    line=$(echo "$line" | tr -d '[:space:]')  # Remove leading and trailing spaces
+    line=$(echo "$line" | tr -d '[:space:]')  # strip leading and trailing spaces
     keys="$keys,$line"
 done < "$csvfile"
 
 keys=${keys:1} # remove leading ','
-
-# echo $keys
 
 export API_KEY="$keys"
 
