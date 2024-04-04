6f1ae87f4ee3d488786ee3958ac370fa9e9ba157
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-04-05 08:17:25 +1000

    new string utils

--- src/utils/string_utils.py
+++ src/utils/string_utils.py
@@ -5,3 +5,14 @@ def mask_string(string: str) -> str:
     string_trimmed = string[:REVEALED_KEY_LEN]
     masked_string = string_trimmed + MASK
     return masked_string
+
+def remove(collection: list[str] | list[list[str]], target: str) -> list[str] | list[list[str]]:
+    new_collection = []
+    for item in collection:
+        if isinstance(item, str):
+            if item != target:
+                new_collection.append(item)
+        elif isinstance(item, list):
+            if target not in item:
+                new_collection.append(item)
+    return new_collection
